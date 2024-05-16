8d6d3059a4fb7b5b8510b376a8cdee4c3cb84d72
Sean Smith <sean@wwsean08.com>
2024-05-16 15:03:43 -0700

    try to add permissions data to the job context

--- src/Runner.Worker/JobContext.cs
+++ src/Runner.Worker/JobContext.cs
@@ -1,11 +1,14 @@
-﻿using GitHub.DistributedTask.Pipelines.ContextData;
+﻿using System.Collections.Generic;
+using GitHub.DistributedTask.Pipelines.ContextData;
 using GitHub.Runner.Common.Util;
 using GitHub.Runner.Common;
+using GitHub.Runner.Sdk;
 
 namespace GitHub.Runner.Worker
 {
     public sealed class JobContext : DictionaryContextData
     {
+        GlobalContext _globalContext;
         public ActionResult? Status
         {
             get
@@ -41,6 +44,30 @@ namespace GitHub.Runner.Worker
             }
         }
 
+        public DictionaryContextData Permissions
+        {
+            get
+            {
+                var permissions = _globalContext.Variables.Get("system.github.token.permissions") ?? "";
+                if (!string.IsNullOrEmpty(permissions))
+                {
+                    var permissionsData = new DictionaryContextData();
+                    this["permissions"] = permissionsData;
+                    var permissionsJson = StringUtil.ConvertFromJson<Dictionary<string, string>>(permissions);
+                    foreach (KeyValuePair<string, string> entry in permissionsJson)
+                    {
+                        permissionsData.Add(entry.Key, new StringContextData(entry.Value));
+                    }
+                    return this["permissions"] as DictionaryContextData;
+                }
+                else
+                {
+                    this["permissions"] = new DictionaryContextData();
+                    return this["permissions"] as DictionaryContextData;
+                }
+            }
+        }
+        
         public DictionaryContextData Container
         {
             get
